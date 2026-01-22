f29289b54d700eb5f23e456eb543e9a25961a184
ehdrb51162 <ehdrb51162@gmail.com>
2026-01-22 15:11:23 +0900

    플레이어 에셋 및 카메라설정 추가

---
+++ Assets/_MY/Scripts/PlayerManager.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StarterAssets;
+using Cinemachine;
+
+public class PlayerManager : MonoBehaviour
+{
+    private StarterAssetsInputs input;
+
+    [Header("Aim")]
+    [SerializeField]
+    private CinemachineVirtualCamera aimCam;
+    
+    [SerializeField]
+    private GameObject aimImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        input = GetComponent<StarterAssetsInputs>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AimCheck();
+    }
+    private void AimCheck()
+    {
+        if(input.aim)
+        {
+            aimCam.gameObject.SetActive(true);
+            aimImage.SetActive(true);
+        }
+        else
+        {
+            aimCam.gameObject.SetActive(false);
+            aimImage.SetActive(false);
+        }
+    }
+}
